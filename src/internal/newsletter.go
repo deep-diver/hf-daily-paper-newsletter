f2f7154a3d4145775e4552e363ceafcdb84fbdf4
Chansung Park <deep.diver.csp@gmail.com>
2024-01-19 16:49:29 +0900

    Update newsletter.go

--- src/internal/newsletter.go
+++ src/internal/newsletter.go
@@ -9,6 +9,7 @@ type Article struct {
 	Tags      []string `yaml:"tags"`
 	Summary   string   `yaml:"summary"`
 	Opinion   string   `yaml:"opinion"`
+	TranslatedPath map[string]string `yaml:"translated_path"`
 }
 
 type Newsletter struct {
