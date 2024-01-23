aa358f89f1909d236d28f8a6b689abe3eab2399d
Chansung Park <deep.diver.csp@gmail.com>
2024-01-23 15:44:26 +0900

    Update update_yaml.py

--- py/update_yaml.py
+++ py/update_yaml.py
@@ -21,6 +21,7 @@ def main(args):
               parsed_yaml.update(
                   {
                       'translated_paths': {
+                          "INT": f"https://raw.githack.com/{args.translated_output_github_repo}/main/{args.translated_output_dir}/{arxiv_id_in_yaml}/{args.non_translated_filename}",
                           "KR": f"https://raw.githack.com/{args.translated_output_github_repo}/main/{args.translated_output_dir}/{arxiv_id_in_yaml}/{args.translated_filename}"
                       }
                   }
@@ -34,6 +35,7 @@ if __name__ == "__main__":
   parser.add_argument('--yaml-dir', type=str, default="current")
   parser.add_argument('--translated-output-github-repo', type=str, default="codingpot/hf-daily-paper-newsletter-tester")
   parser.add_argument('--translated-output-dir', type=str, default="translated-papers")
+  parser.add_argument('--non-translated-filename', type=str, default="paper.en.html")
   parser.add_argument('--translated-filename', type=str, default="paper.ko.html")
 
   args = parser.parse_args()
